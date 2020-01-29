5b54e5a35136a1e6af25bcb004d39e4254074d32
unknown <udaylunawat@gmail.com>
2020-01-29 13:26:34 +0530

    Updated 2nd answer, removed unnecessary files

--- 2) Operating a sorted array.py
+++ 2) Operating a sorted array.py
@@ -1,4 +1,7 @@
-#Search operation
+# Search operation using Binary search
+# Search: O(Log n) [Using Binary Search]
+# Insert: O(n) [In worst case all elements may have to be moved]
+# Delete: O(n) [In worst case all elements may have to be moved]
 
 def binarySearch(arr, low, high, key):
     if high < low:
@@ -17,6 +20,34 @@ def binarySearch(arr, low, high, key):
     else:
         return binarySearch(arr, low, mid-1, key)
 
+#Insert operation
+
+def insertSorted(array, pos, key, capacity):
+    if pos >= capacity:
+        return pos
+    
+    i = n-1
+    while i >= 0 and arr[i] > key: 
+        arr[i + 1] = arr[i] 
+        i -= 1
+
+    arr[i+1] = key
+    return (n + 1)
+
+def deleteElement(arr, n, key):
+    index = binarySearch(arr, 0, n-1, key)
+
+    if index == -1:
+        return n
+    
+    #Shifting elements larger elements to the left
+    for i in range(int(index),n-1):
+        arr[i] = arr[i+1]
+
+    return n-1
+
+
+
 
 
 # Driver program to check above functions  
@@ -25,3 +56,37 @@ arr = [5, 6, 7, 8, 9, 10]
 n = len(arr) 
 key = 10
 print("Index:", int(binarySearch(arr, 0, n, key) )) 
+
+
+
+for i in range(20): 
+    arr.append(0) 
+  
+capacity = len(arr) 
+n = 6
+key = 26
+  
+print("Before Insertion: ", end = " "); 
+for i in range(n): 
+    print(arr[i], end = " ") 
+      
+# Inserting key 
+n = insertSorted(arr, n, key, capacity) 
+  
+print("\nAfter Insertion: ", end = "") 
+for i in range(n): 
+    print(arr[i], end = " ") 
+
+
+
+print("\nBefore Deletion: ", end = " "); 
+for i in range(n): 
+    print(arr[i], end = " ") 
+      
+# Deleting key
+key = 26 
+n = deleteElement(arr, n, key) 
+  
+print("\nAfter Deletion: ", end = "") 
+for i in range(n): 
+    print(arr[i], end = " ") 
